985e4de039e09db559b4be600b346b9c35914c65
henningrm <henning.mielkau@gmail.com>
2023-11-03 14:26:48 +0800

    initial array greet created

---
+++ mixed-messages.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+
+
+//CREATING THE CONTENT ARRAYS
+
+/* Greet Array
+Structure: greet = [[[DE1,name],[DE2,name],[DE3,name]],[[EN1,name],[EN2,name],[EN3,name]]]
+*/
+
+greet = [
+    [
+        ['Hey','NAME','!'],['Helloo','NAME','!'],['NAME','alte Hütte!']
+    ],
+    [
+        ['Hey','NAME','"'],['NAME', 'my dear!'],['Deares','NAME','!']
+    ]
+];
+
+
+
+//COMBINING THE MODULES
